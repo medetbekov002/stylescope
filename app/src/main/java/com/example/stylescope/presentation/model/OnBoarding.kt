5fe0c06f5662398d130db851279540d0ae30a4a0
Dom <DANIELPHP01>
2023-07-10 01:50:23 +0600

    fix filter

--- app/src/main/java/com/example/stylescope/presentation/model/OnBoarding.kt
+++ app/src/main/java/com/example/stylescope/presentation/model/OnBoarding.kt
@@ -4,5 +4,6 @@ data class OnBoarding(
     val isBack:Boolean,
     val title:String,
     val desc:String,
-    val img:Int
+    val img:Int,
+    val isLast:Boolean
 )
--- app/src/main/java/com/example/stylescope/presentation/ui/fragments/onboarding/OnBoardingAdapter.kt
+++ app/src/main/java/com/example/stylescope/presentation/ui/fragments/onboarding/OnBoardingAdapter.kt
@@ -3,31 +3,37 @@ package com.example.stylescope.presentation.ui.fragments.onboarding
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.stylescope.R
 import com.example.stylescope.databinding.ItemOnBoardingBinding
 import com.example.stylescope.presentation.model.OnBoarding
+import com.google.android.material.textview.MaterialTextView
 
-class OnBoardingAdapter() : RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>() {
+class OnBoardingAdapter(private val click: () -> Unit,private val backClick: () -> Unit,private val lastClick: () -> Unit) : RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>() {
 
     private val arrayList = arrayListOf(
         OnBoarding(
             isBack = false,
             title = "Удобство",
             desc = "Мы предоставляем удобный и быстрый способ найти лучшие дизайнерские услуги в Кыргызстане и получить профессиональную помощь в создании уникального пространства.",
-            img = R.drawable.on_boarding_img_1
+            img = R.drawable.on_boarding_img_1,
+            false
         ),
         OnBoarding(
             isBack = true,
             title = "Качество",
             desc = "Мы тщательно отбираем компании и дизайнеров, чтобы предоставлять только высококачественные услуги, гарантируя тем самым, что наши клиенты получат лучшее в своей отрасли",
-            img = R.drawable.on_boarding_img_2
+            img = R.drawable.on_boarding_img_2,
+            false
         ),
         OnBoarding(
             isBack = true,
             title = "Выбор",
             desc = "Мы предоставляем широкий спектр услуг дизайна интерьера и экстерьера компаний, включая проектирование, меблировку, подбор материалов, декорирование, ремонт, обучение.",
-            img = R.drawable.on_boarding_img_3
+            img = R.drawable.on_boarding_img_3,
+            true
         ),
     )
 
@@ -54,10 +60,14 @@ class OnBoardingAdapter() : RecyclerView.Adapter<OnBoardingAdapter.OnBoardingVie
         fun bind(onBoarding: OnBoarding) {
             with(binding) {
                 btnBack.setOnClickListener {
-
+                    backClick.invoke()
                 }
                 btnNext.setOnClickListener {
-
+                  if (arrayList.last().isLast) {
+                      lastClick.invoke()
+                  } else {
+                      click.invoke()
+                  }
                 }
                 tvTitle.text = onBoarding.title
                 tvDesc.text = onBoarding.desc
